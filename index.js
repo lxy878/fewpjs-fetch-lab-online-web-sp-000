04b761f71fbab92b8ff54190c044548ccf39b1ee
Xiaoyi Liu <lxy878@gmail.com>
2020-08-14 12:25:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function fetchBooks() {
-  fetch('https://anapioficeandfire.com/api/books').then(resp => {
-    return resp.json();
+  return fetch('https://anapioficeandfire.com/api/books').then(resp => {
+    resp.json();
   }).then(json => {
     renderBooks(json);
   });
